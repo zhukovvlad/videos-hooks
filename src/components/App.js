a1b63e6d170f0dc339f4a9c408ab8896391a03f6
Zhukovvlad <zhukovvlad@yandex.ru>
2022-05-20 17:52:33 +0300

    Created custom Hook for fetch videos

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,32 @@
 import React, { useState, useEffect } from "react";
 
 import SearchBar from "./SearchBar";
-import youtube from "../api/youtube";
 import VideoList from "./VideoList";
 import VideoDetail from "./VideoDetail";
+import useVideos from "../hooks/useVideos";
 
 const App = () => {
-    const [videos, setVideos] = useState([]);
     const [selectedVideo, setSelectedVideo] = useState(null);
+    const [videos, search] = useVideos('buildings');
 
     useEffect(() => {
-        onTermSubmit('buildings');
-    }, []);
-
-    const onTermSubmit = async term => {
-        const response =  await youtube.get('/search', {
-            params: {
-                q: term
-            }
-        });
-
-        setVideos(response.data.items);
-        setSelectedVideo(response.data.items[0]);
-    };
-
-    const onVideoSelect = (video) => {
-        setSelectedVideo(video);
-    };
+        setSelectedVideo(videos[0]);
+    }, [videos]);
+    
 
     return (
         <div className="ui container" style={{ marginTop: '10px' }}>
-            <SearchBar onFormSubmit={onTermSubmit} />
+            <SearchBar onFormSubmit={search} />
             <div className="ui grid">
                 <div className="ui row">
                     <div className="eleven wide column">
                         <VideoDetail video={selectedVideo} />
                     </div>
                     <div className="five wide column">
-                        <VideoList videos={videos} onVideoSelect={onVideoSelect} />
+                        <VideoList
+                            videos={videos}
+                            onVideoSelect={setSelectedVideo}    // we don't need put arrow function like (video) => setSelectedVideo(video)
+                        />
                     </div>
                 </div>
             </div>
